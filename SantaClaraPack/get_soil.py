b8fd26307b9573742f617bf9a272b982783e9ead
anderson-visconti <anderson.svisconti@gmail.com>
2019-09-15 13:57:18 -0300

    v0.05

---
+++ SantaClaraPack/get_soil.py
@@ -0,0 +1,76 @@
+import requests
+import os
+import netCDF4
+import numpy as np
+import pandas as pd
+from SantaClaraPack.Config.Config import Config
+from SantaClaraPack.Banco.Dados import Dados
+
+config = Config().config
+df = pd.DataFrame()
+
+soil_path = "soilw.mon.mean.v2.nc"
+
+raw_data = netCDF4.MFDataset(soil_path)
+
+lats = raw_data.variables['lat'][:]
+lons = raw_data.variables['lon'][:]
+soils = raw_data.variables['soilw'][:]
+temps = raw_data.variables["time"][:]
+
+temps = netCDF4.num2date(
+    times=raw_data.variables[config['soil']['variables_name']['tempo']][:],
+    units=raw_data.variables['time'].units
+)
+
+# Recorta os dados para o subset estipulado
+lat_inds = np.where(
+    (lats >= config['soil']['sub_set']['lat']['ini']) &
+    (lats <= config['soil']['sub_set']['lat']['fim'])
+)
+
+lon_inds = np.where(
+    (lons >= config['soil']['sub_set']['lon']['ini']) &
+    (lons <= config['soil']['sub_set']['lon']['fim'])
+)
+
+latf=[]
+lonf=[]
+
+'''
+for lat in lats:
+    if lat in np.arange(-22, -17, 0.25):
+        latf.append(lat)
+    else:
+        pass
+
+
+for lon in lons:
+    if lon in np.arange(127.75, 140.25, 0.25):
+        lonf.append(lon)
+    else:
+        pass
+'''
+
+lats = raw_data.variables['lat'][lat_inds]
+lons = raw_data.variables['lon'][lon_inds]
+soils = raw_data.variables['soilw'][:, lat_inds[0], lon_inds[0]]
+
+for i, tempo in enumerate(temps):
+
+    for j, lat in enumerate(lats):
+        aux = dict(
+            dat_medicao=[tempo] * len(lons),
+            val_lat=[lat] * len(lons),
+            val_lon=lons[:],
+            val_soil=soils[i, j, :]
+        )
+        aux = pd.DataFrame.from_dict(data=aux, orient='columns')
+        df = pd.DataFrame(pd.concat(objs=[df, aux]))
+
+
+# Insere no banco de dados
+dados = Dados()
+dados.insert_solo(df=df)
+
+print(df)
