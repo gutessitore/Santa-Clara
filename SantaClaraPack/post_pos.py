2470a947ba8fcfdcd294565b0bac93a0f05338e1
gutessitore <you@example.com>
2019-10-10 22:04:47 -0300

    v0.06

--- SantaClaraPack/post_pos.py
+++ SantaClaraPack/post_pos.py
@@ -3,6 +3,7 @@ from Banco.Dados import Dados
 from Config.Config import Config
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
+import matplotlib
 import numpy as np
 
 dados = Dados()
@@ -25,8 +26,17 @@ postos = dados.get_posto()
 # print(distance.euclidean(lat_lon[0], lat_lon[1])) #0.013315404612704994
 
 fig = plt.figure()
-ax1 = fig.add_subplot(111)
+ax = fig.add_subplot(111)
 
-ax1.scatter(rio.val_lon, rio.val_lat, s=2, c='b')
-ax1.scatter(postos.num_lon ,postos.num_lat, s=5, c='r', marker="v")
+circulo = plt.matplotlib.patches.Ellipse((postos.num_lon[0]+0.1, postos.num_lat[0]-0.7),
+											 width=0.82, 
+											 height=1.4,
+											 alpha=0.5,
+											 color='darkcyan')
+
+
+ax.scatter(rio.val_lon, rio.val_lat, s=2, c='b')
+ax.add_artist(circulo)
+ax.scatter(postos.num_lon ,postos.num_lat, s=200, c='r', marker="v")
+plt.grid()
 plt.show()
