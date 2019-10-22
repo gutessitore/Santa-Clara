7e159bcb25f7244c93beee0b723bd4371aab85bc
anderson-visconti <anderson.svisconti@gmail.com>
2019-10-22 18:50:59 -0300

    Add files via upload

--- SantaClaraPack/models_avaliation.py
+++ SantaClaraPack/models_avaliation.py
@@ -1,21 +1,28 @@
 import optuna
+import warnings
 from joblib import dump, load
-from sklearn.metrics import mean_absolute_error, median_absolute_error, r2_score
+from sklearn.metrics import mean_absolute_error, r2_score
 from SantaClaraPack.Models.PreProcessors import *
 from SantaClaraPack.Banco.Dados import Dados
 from SantaClaraPack.Optimizer.Optimizer import Optimizer
-from SantaClaraPack.Plot.Plot import Plot
+
 
 if __name__ == '__main__':
+
+    warnings.filterwarnings('ignore')
     desired_width = 320
     pd.set_option('display.width', desired_width)
     pd.set_option('display.max_columns', 100)
 
 
+    # Numero de dias de previsão
+    n_outs = 7
+
     dao = Dados()
     df_vazao = dao.get_vazao(posto=1, classe='Vazao', data_inicial='2000-01-01', data_final='2016-12-31')
     df_vazao = pd.DataFrame(df_vazao.iloc[:, 1:4])
     df_vazao.drop(columns=['num_posto'], inplace=True)
+    df_vazao['dat_medicao'] = pd.to_datetime(df_vazao['dat_medicao'])
     df_vazao.set_index(keys=['dat_medicao'], inplace=True)
 
     df_chuva = dao.get_gridded_data(
@@ -55,13 +62,13 @@ if __name__ == '__main__':
 
     study = optuna.create_study(
         storage='sqlite:///Optimizer/optimize_tests.db',
-        direction='maximize',
+        direction='minimize',
         study_name='optimize',
         load_if_exists=True
     )
 
-    optimize.get_data(X=X_train, y=y_train, X_test=X_test, y_test=y_test)
-    study.optimize(optimize, n_trials=50)
+    optimize.get_data(X=X_train, y=y_train, X_test=X_test, y_test=y_test, n_outs=n_outs)
+    #study.optimize(optimize, n_trials=50, n_jobs=1)
 
     # Cria e salva melhor configuração
     model = optimize.create_best_model(params=study.best_params)
@@ -75,16 +82,32 @@ if __name__ == '__main__':
         X=X_test,
         y=y_test,
         n_in=study.best_params['window_neg'],
-        n_out=0
+        n_out=n_outs
     )
 
     y_hat = model.predict(X=X_test_lag)
+    df_y_hat = pd.DataFrame(data=y_hat, index=X_test_lag.index)
+
+    df_previsao = pd.DataFrame()
+    for i, row in df_y_hat.iterrows():
+        aux = pd.DataFrame(
+                data=dict(
+                    dat_reference=i,
+                    dat_previsao=pd.date_range(start=i, freq='D', periods=n_outs),
+                    val_vaz_prevista=row.values,
+                    num_termo=[i for i in range(1, n_outs + 1)],
+                    val_vaz_natr=df_vazao.loc[pd.date_range(start=i, freq='D', periods=n_outs), 'val_vaz_natr']
+                )
+        )
+
+        df_previsao = pd.concat(objs=[df_previsao, aux])
+
+    df_previsao.to_csv(path_or_buf=r'Fig/previsao_posto_1_oneshot.csv', sep=';', decimal=',')
 
     # Scores
     print('MAE test: {:}'.format(mean_absolute_error(y_true=y_test_lag, y_pred=y_hat)))
-    print('MedAE test: {:}'.format(median_absolute_error(y_true=y_test_lag, y_pred=y_hat)))
     print('R2 test: {:}'.format(r2_score(y_true=y_test_lag, y_pred=y_hat)))
 
     # plot
-    plot = Plot()
-    plot.plot_prediction_compararison(y_true=y_test_lag['val_vaz_natr'].values, y_pred=y_hat, times=y_test_lag.index)
+    #plot = Plot()
+    #plot.plot_prediction_compararison(y_true=y_test_lag['val_vaz_natr'].values, y_pred=y_hat, times=y_test_lag.index)
